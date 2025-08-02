fd2ece3dfca0a28ec93f0d3ee5d40f7297eaecf1
DouglasAdriel01 <douglas52528996@aluno.ms.senac.br>
2025-08-01 21:16:58 -0400

    codigo_professor

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./componetes/Home";
 import DetalhesPrato from "./componetes/DetalhesPrato";
 import FormularioPrato from "./componetes/FormularioPrato";
+import Login from "./componetes/Login";
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AuthProvider><Home /></AuthProvider>} />
         <Route path="/detalhes-prato/:id" element={<DetalhesPrato />} />
         <Route path="/cadastro-prato" element={<FormularioPrato />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
